350e88155361dbf7b24c6c37cf896fce762190b7
Joodler <amistsss@163.com>
2018-10-05 00:43:23 +0800

    v 0.2.1 update

--- R/data.R
+++ R/data.R
@@ -1,15 +1,17 @@
-#' Long-format of queen spatial weight matrix
+#' Long-format spatial weight matrix of Queen
 #'
-#' Long-format of queen spatial weight matrix
+#' Long-format spatial weight matrix of Queen
 #' @format A data frame with 60516 rows and 7 columns.
 #' \describe{
-#'   \item{Country Name}{Country Name}
-#'   \item{Country ISO3 Code}{Country Name}
-#'   \item{Country UN Code}{Country Name}
-#'   \item{Counterpart Country Name}{Country Name}
-#'   \item{Counterpart Country ISO3 Code}{Country Name}
-#'   \item{Counterpart Country UN Code}{Country Name}
-#'   \item{Contiguity}{Country Name}
+#'   \item{Country Name}{Country name}
+#'   \item{Country ISO3 Code}{ISO3 country code corresponding to the country name}
+#'   \item{Country UN Code}{UN country code corresponding to the country name}
+#'   \item{Counterpart Country Name}{Counterpart country name}
+#'   \item{Counterpart Country ISO3 Code}{ISO3 counterpart country code corresponding to \\
+#'   the counterpart country name}
+#'   \item{Counterpart Country UN Code}{UN counterpart country code corresponding to \\
+#'   the counterpart country name}
+#'   \item{Contiguity}{Wether the country and the counterpart country are neighbors}
 #' }
 #' @keywords internal
 "queen_long"
